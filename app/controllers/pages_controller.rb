1b6f573ecbd838565f57b5d6b1b86994b577c9cb
albacr <carrascal.marina@gmail.com>
2016-08-10 07:34:18 -0500

    proyecto

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def porfolio
+  end
+
+  def contact
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+
+  root 'pages#home'
+
+
+  get 'about'=> 'pages#about', as: :about
+  get 'services'=> 'pages#services', as: :services
+  get 'contact' => 'pages#contact', as: :contact
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
